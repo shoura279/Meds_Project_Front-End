587dee84cddd99994e6009165f02b0cd551ff51e
shoura279 <ahmedshoura279@gmail.com>
2023-05-04 11:38:26 +0300

    web load

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { routes } from "./routes";
+import { routes } from "./src/routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
